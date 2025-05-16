e3bb0b6904f4da966c2f7088b37911406273dbac
geka873 <gekaalcontinn@gmail.com>
2025-05-17 03:29:08 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Panel1 = New Panel()
+        PictureBox2 = New PictureBox()
+        PictureBox1 = New PictureBox()
+        LinkLabel2 = New LinkLabel()
+        LinkLabel1 = New LinkLabel()
+        Label3 = New Label()
+        TextBox2 = New TextBox()
+        Label2 = New Label()
+        PictureBox3 = New PictureBox()
+        Panel1.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = SystemColors.ControlLightLight
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = Color.Black
+        Button1.Location = New Point(107, 266)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(113, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Login"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(50, 99)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(296, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.BackColor = Color.Transparent
+        Label1.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Black
+        Label1.Location = New Point(140, 17)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(47, 20)
+        Label1.TabIndex = 2
+        Label1.Text = "Login"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.DarkGray
+        Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), Image)
+        Panel1.BackgroundImageLayout = ImageLayout.Stretch
+        Panel1.Controls.Add(PictureBox2)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Controls.Add(LinkLabel2)
+        Panel1.Controls.Add(LinkLabel1)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.ForeColor = Color.White
+        Panel1.Location = New Point(26, 100)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(356, 375)
+        Panel1.TabIndex = 3
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(5, 175)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(39, 37)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 9
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.ErrorImage = CType(resources.GetObject("PictureBox1.ErrorImage"), Image)
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(5, 89)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(39, 37)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 8
+        PictureBox1.TabStop = False
+        ' 
+        ' LinkLabel2
+        ' 
+        LinkLabel2.AutoSize = True
+        LinkLabel2.BackColor = Color.Transparent
+        LinkLabel2.DisabledLinkColor = Color.White
+        LinkLabel2.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel2.LinkColor = Color.Black
+        LinkLabel2.Location = New Point(91, 316)
+        LinkLabel2.Name = "LinkLabel2"
+        LinkLabel2.Size = New Size(148, 20)
+        LinkLabel2.TabIndex = 7
+        LinkLabel2.TabStop = True
+        LinkLabel2.Text = "Create New Account"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.ActiveLinkColor = Color.Black
+        LinkLabel1.AutoSize = True
+        LinkLabel1.BackColor = Color.Transparent
+        LinkLabel1.DisabledLinkColor = Color.White
+        LinkLabel1.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        LinkLabel1.ForeColor = Color.White
+        LinkLabel1.LinkColor = Color.Black
+        LinkLabel1.Location = New Point(107, 234)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(123, 20)
+        LinkLabel1.TabIndex = 6
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "Forgot Password"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Transparent
+        Label3.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Black
+        Label3.Location = New Point(50, 162)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(73, 20)
+        Label3.TabIndex = 5
+        Label3.Text = "Password"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(50, 185)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(296, 27)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.BackColor = Color.Transparent
+        Label2.Font = New Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.ForeColor = Color.Black
+        Label2.Location = New Point(50, 76)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(78, 20)
+        Label2.TabIndex = 3
+        Label2.Text = "Username"
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Dock = DockStyle.Fill
+        PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), Image)
+        PictureBox3.Location = New Point(0, 0)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(1119, 608)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 10
+        PictureBox3.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1119, 608)
+        Controls.Add(Panel1)
+        Controls.Add(PictureBox3)
+        Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Name = "Form1"
+        Text = "Car Information System"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+
+End Class
